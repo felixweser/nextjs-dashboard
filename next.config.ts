0d4694ec8eee48ce6879bafc98aa99effed27ee6
felixweser <f8938828@gmail.com>
2025-01-30 10:23:30 +0100

    No clue

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experiemental: {
+    ppr: 'incremental'
+  }
 };
 
 export default nextConfig;
